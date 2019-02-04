797b26ec005fe14e26be64b0c25c6e91845296bb
Kelvin Mai <kelvin.mai002@gmail.com>
2019-02-03 20:04:08 -0500

    Updated clojure fibonacci solution for sanity

--- fibonacci/solution.clj
+++ fibonacci/solution.clj
@@ -1,11 +1,17 @@
 ; using recursion
+(defn build-fib
+  [x]
+  (conj x (+ (last x) (nth x (- (count x) 2)))))
+
 (defn fib
-  ([n]
-     (fib [0 1] n))
-  ([x, n]
-     (if (<= (count x) n) 
-       (fib (conj x (+ (last x) (nth x (- (count x) 2)))) n)
-       x)))
+  ([num] (fib [0 1] num))
+  ([fib-list num]
+   (if (<= (count fib-list) num)
+     (recur (build-fib fib-list) num)
+     fib-list)))
+
+(defn fib-num [num]
+  (last (fib num)))
 
 ; test case of 15
 (fib 15)
